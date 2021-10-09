6b7feea5bb402ccba95203306f2625a824d30017
Jianzhi Wang <jianzhi@berkeley.edu>
2021-10-08 22:56:20 -0700

    added mui lab and timeline

--- frontend/src/Issues/IssueComponent.js
+++ frontend/src/Issues/IssueComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useParams } from "react-router";
-
+import CustomizedTimeline from "./CustomizedTimeline.js"
 export default function IssueComponent() {
     let { id } = useParams()
     return (
         <div>
+            <CustomizedTimeline/>
             <h1>Issue</h1>
             <h1>{id}</h1>
         </div>
